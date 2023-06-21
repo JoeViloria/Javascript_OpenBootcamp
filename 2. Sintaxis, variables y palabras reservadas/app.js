e21e185bb1a005810fa3332e70ae4fb0fb4d7dfb
Joe Viloria <joeviloria12@hotmail.com>
2023-06-21 14:14:40 +0800

    Add files via upload

---
+++ 2. Sintaxis, variables y palabras reservadas/app.js
@@ -0,0 +1,10 @@
+let nombre = "Joe";
+let edad = 28;
+let esDesarrollador = true;
+const fechaNacimiento = new Date("1990-01-01");
+const libroFavorito = {
+  titulo: "El libro favorito",
+  autor: "Autor del libro",
+  fecha: new Date("2021-01-01"),
+  url: "https://ejemplo.com/libro-favorito",
+};
