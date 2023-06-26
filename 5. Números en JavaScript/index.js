0270f9d76fb74485c89f90b31dc3d0573e689fc6
Joe Viloria <joeviloria12@hotmail.com>
2023-06-26 16:30:33 +0800

    Add files via upload

---
+++ 5. Números en JavaScript/index.js
@@ -0,0 +1,17 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+let altura_cm = 188;
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+let altura_m = 1.88;
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso_kg = 73.2;
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRound = Math.ceil(altura_m);
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRound = Math.floor(peso_kg);
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let maximoIgual = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
